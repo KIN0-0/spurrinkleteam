1027d9d5e72740b3677a70af42c4598c7545e03f
Gabal Gim <97859625+KIN0-0@users.noreply.github.com>
2022-11-14 14:13:34 +0900

    Initial commit

---
+++ src/test/java/com/example/spurrinkleteam/SpurrinkleteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spurrinkleteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpurrinkleteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
